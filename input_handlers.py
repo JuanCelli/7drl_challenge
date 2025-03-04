d45fcee72b17fbd36910d7e8a2a6422b07ccff42
jifernandez7drl <jifernandez7drl@gmail.com>
2025-03-04 18:18:31 -0300

    Reemplazar KeySym

--- input_handlers.py
+++ input_handlers.py
@@ -105,7 +105,7 @@ class GameOverEventHandler(EventHandler):
 
         key = event.sym
 
-        if key == tcod.event.K_ESCAPE:
+        if key == tcod.event.KeySym.ESCAPE:
             action = EscapeAction(self.engine.player)
 
         return action
